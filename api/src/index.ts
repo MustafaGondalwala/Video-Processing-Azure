01959c5051ef53bce3d9460bad9ba3af11f467ac
Mustafa G <mustafagondalwala32@gmail.com>
2024-07-28 18:41:43 +0530

    added `morgan` middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,11 +3,13 @@ import videoController from "./controllers/video.controller";
 import uploadMiddleware from "./middlewares/upload";
 import logger from "./middlewares/logger";
 import config from "./config";
+import morganMiddleware from "./middlewares/morgan";
 
 const app = express();
 const PORT = config.port;
 
 app.use(express.json());
+app.use(morganMiddleware); // Add morgan middleware
 
 app.post("/upload", uploadMiddleware, (req, res) =>
   videoController.upload(req, res)
